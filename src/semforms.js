ef764661e11e910b8778dbd584ddf710c5b2cb8e
Miroslav Blasko <blaskmir@fel.cvut.cz>
2016-11-27 17:42:41 +0100

    [new] formutils export

--- src/semforms.js
+++ src/semforms.js
@@ -2,3 +2,4 @@ export WizardGenerator from './model/WizardGenerator';
 export Configuration from './model/Configuration';
 export QuestionAnswerProcessor from './model/QuestionAnswerProcessor';
 export Constants from './constants/Constants';
+export FormUtils from './util/FormUtils'; //todo should not be exported !?
