24d09fd6d9652934b3d48d55021b67b96dacb63d
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-05-15 13:40:01 +0200

    Use the latest version of typeahead, which supports disabling.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import React from "react";
 import assign from "object-assign";
 import {Glyphicon, Panel} from "react-bootstrap";
--- src/components/answer/TypeaheadAnswer.js
+++ src/components/answer/TypeaheadAnswer.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import React from 'react';
 import JsonLdUtils from 'jsonld-utils';
 import Typeahead from 'react-bootstrap-typeahead';
