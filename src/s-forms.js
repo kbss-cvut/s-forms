dbfc448be754ccbe71c234c2fbbb5f17966fa76c
tomasklima <tomas.klim@gmail.com>
2020-07-01 22:30:09 +0200

    [upd] Export utils and constants from library

--- src/s-forms.js
+++ src/s-forms.js
@@ -1,3 +1,9 @@
 import SForms from './components/SForms';
+import * as Constants from './constants/Constants';
+import JsonLdObjectUtils from './util/JsonLdObjectUtils';
+import JsonLdFramingUtils from './util/JsonLdFramingUtils';
+import JsonLdObjectMap from './util/JsonLdObjectMap';
+import FormUtils from './util/FormUtils';
 
 export default SForms;
+export { Constants, JsonLdObjectUtils, JsonLdFramingUtils, JsonLdObjectMap, FormUtils };
