202e5b3212fc12ebf56ed18e065fc31edd5b9e87
tomasklima <tomas.klim@gmail.com>
2020-07-10 10:26:16 +0200

    [Fix] Vertical wizard nav + accomplish TODO

--- src/components/wizard/VerticalWizardNav.jsx
+++ src/components/wizard/VerticalWizardNav.jsx
@@ -3,20 +3,23 @@ import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import JsonLdUtils from 'jsonld-utils';
 import { ConfigurationContext } from '../../contexts/ConfigurationContext';
+import FormUtils from '../../util/FormUtils';
 
-//TODO add "disabled" to VerticalWizardNav
 const VerticalWizardNav = ({ steps, onNavigate, currentStep }) => {
   const { options } = useContext(ConfigurationContext);
 
   return (
-    <div className="wizard-nav col-2 p-0">
+    <div className="wizard-nav col-2 p-0 pr-2">
       <ListGroup>
         {steps.map((step, index) => (
           <ListGroupItem
+            disabled={!FormUtils.isRelevant(step)}
             key={'nav' + index}
             onClick={() => onNavigate(index)}
             id={'wizard-nav-' + index}
+            action={true}
             active={index === currentStep ? 'active' : ''}
+            variant={'default'}
           >
             {JsonLdUtils.getLocalized(step[JsonLdUtils.RDFS_LABEL], options.intl)}
           </ListGroupItem>
