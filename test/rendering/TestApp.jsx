8d628d6429f7fc989439c170cb6aca4528decf63
Miroslav Blasko <blcham@gmail.com>
2020-10-04 14:18:43 +0200

    [New] Support rendering test with startingQuestionId query parameter

--- test/rendering/TestApp.jsx
+++ test/rendering/TestApp.jsx
@@ -4,9 +4,24 @@ import SForms from '../../src/components/SForms';
 
 import '../../src/styles/s-forms.css';
 import 'react-datepicker/dist/react-datepicker.css';
+import queryString from 'query-string'
 
 const form1 = require('./form1.json'); // form with wizard steps
 const form2 = require('./form2.json'); // form without wizard steps (proudly assembled in Semantic Form Web Editor)
+
+const getP = (queryParameterName, defaultValue) => {
+  return {
+    [queryParameterName]: getQueryParameter(queryParameterName, defaultValue)
+  }
+}
+
+const getQueryParameter = (parameterName, defaultValue) => {
+  const value = queryString.parse(window.location.search)[parameterName];
+  if (value) {
+    return value;
+  }
+  return defaultValue;
+}
 
 class TestApp extends React.Component {
   constructor(props) {
@@ -41,9 +56,10 @@ class TestApp extends React.Component {
       },
       modalView: false,
       modalProps,
-      horizontalWizardNav: true,
+      horizontalWizardNav: false,
       wizardStepButtons: true,
       enableForwardSkip: true,
+      ...getP('startingQuestionId', "layout-options-65"),
       startingStep: 1
     };
 
