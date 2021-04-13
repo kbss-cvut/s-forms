092af624f93d33756a84b33050500cab67875103
holubv <holubvojtech@gmail.com>
2021-04-13 21:27:22 +0200

    Export classes

--- src/s-forms.js
+++ src/s-forms.js
@@ -4,6 +4,24 @@ import JsonLdObjectUtils from './util/JsonLdObjectUtils';
 import JsonLdFramingUtils from './util/JsonLdFramingUtils';
 import JsonLdObjectMap from './util/JsonLdObjectMap';
 import FormUtils from './util/FormUtils';
+import Question from './components/Question';
+import { ConfigurationContext } from './contexts/ConfigurationContext';
+import Answer from './components/Answer';
+import ComponentRegistry from './util/ComponentRegistry';
+import HelpIcon from './components/HelpIcon';
+import WizardStep from './components/wizard/WizardStep';
 
 export default SForms;
-export { Constants, JsonLdObjectUtils, JsonLdFramingUtils, JsonLdObjectMap, FormUtils };
+export {
+  Constants,
+  JsonLdObjectUtils,
+  JsonLdFramingUtils,
+  JsonLdObjectMap,
+  FormUtils,
+  Question,
+  Answer,
+  HelpIcon,
+  ConfigurationContext,
+  ComponentRegistry,
+  WizardStep
+};
