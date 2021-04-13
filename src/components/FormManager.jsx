0afbca44e0e490dd32b354e3d01636f8b12845fe
holubv <holubvojtech@gmail.com>
2021-04-13 21:32:09 +0200

    Move smart components to separate project

--- src/components/FormManager.jsx
+++ src/components/FormManager.jsx
@@ -3,47 +3,11 @@ import QuestionAnswerProcessor from '../model/QuestionAnswerProcessor';
 import { FormQuestionsContext } from '../contexts/FormQuestionsContext';
 import Wizard from './wizard/Wizard';
 import { FormUtils } from '../s-forms';
-import Question from './Question';
 import FormWindow from './FormWindow';
 import Card from 'react-bootstrap/Card';
-import JsonLdUtils from 'jsonld-utils';
-import Constants from '../constants/Constants';
-import CompositeQuestion from './CompositeQuestion';
 import ComponentRegistry from '../util/ComponentRegistry';
-import QuestionWithAdvanced from './QuestionWithAdvanced';
-import WizardStepWithAdvanced from './wizard/WizardStepWithAdvanced';
 
 class FormManager extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    ComponentRegistry.registerComponent(
-      CompositeQuestion,
-      q => JsonLdUtils.getJsonAttValue(q, Constants.COMPOSITE_PATTERN)
-    );
-
-    const advancedQuestionMap = q => {
-
-      if (!FormUtils.isSection(q) && !FormUtils.isAnswerable(q)) {
-        return false;
-      }
-      let subQuestions = q[Constants.HAS_SUBQUESTION];
-      if (subQuestions && subQuestions.length) {
-        for (let subQuestion of subQuestions) {
-          if (JsonLdUtils.hasValue(subQuestion, Constants.SHOW_ADVANCED_QUESTION, true)) {
-            return true;
-          }
-        }
-      }
-      return false;
-
-    };
-
-    ComponentRegistry.registerComponent(QuestionWithAdvanced, advancedQuestionMap);
-    ComponentRegistry.registerWizardStep(WizardStepWithAdvanced, advancedQuestionMap);
-  }
-
   getFormData = () => {
     const data = this.context.getData();
     const formQuestionsData = this.context.getFormQuestionsData();
