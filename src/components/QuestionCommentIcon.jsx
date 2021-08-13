2a2bf02d5efe92a362dcb6daf923b991772e7b17
chopamax <chopamax@fd.cvut.cz>
2021-08-13 09:40:16 +0200

    [enhancement #51] Layout of QuestionComment implementation

---
+++ src/components/QuestionCommentIcon.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import QuestionComment from "../styles/icons/QuestionComment";
+
+const QuestionCommentIcon = () => {
+    return(
+        <QuestionComment />
+    )
+}
+
+export default QuestionCommentIcon
