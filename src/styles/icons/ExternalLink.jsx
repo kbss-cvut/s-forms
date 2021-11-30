a42757016b57a7b3b51c81976483b9bd3c453c58
chopamax <chopamax@fd.cvut.cz>
2021-11-30 11:28:05 +0100

    [Fix] Make link icon wider

--- src/styles/icons/ExternalLink.jsx
+++ src/styles/icons/ExternalLink.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ExternalLink = () => (
     <svg
         className="external-link-icon"
-        width="16"
+        width="24"
         height="16"
         viewBox="0 0 16 16"
         xmlns="http://www.w3.org/2000/svg">
