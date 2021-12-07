20bd3449907335dd6999453d04fd37a133d688d0
chopamax <chopamax@fd.cvut.cz>
2021-12-07 11:54:18 +0100

    [Upd #63] add tooltip to get full author id and exact comment date

--- src/components/comment/CommentView.jsx
+++ src/components/comment/CommentView.jsx
@@ -2,16 +2,18 @@ import React, {useContext, useState} from "react";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 import {ConfigurationContext} from "../../contexts/ConfigurationContext";
+import LinkIcon from "../LinkIcon";
+import IconOverlay from "../IconOverlay";
+import PropTypes from "prop-types";
 
 const UNKNOWN_AUTHOR = "Unknown author";
 
 const CommentView = (props) => {
     const { options } = useContext(ConfigurationContext);
+    const [showIRI, setShowIRI] = useState(false);
 
     TimeAgo.addLocale(en);
     const time = new TimeAgo('en-US');
-
-    const [showIRI, setShowIRI] = useState(false);
 
     const getAuthorLabel = () => {
         const users = options.users;
@@ -42,27 +44,58 @@ const CommentView = (props) => {
         return UNKNOWN_AUTHOR;
     }
 
-    const getTimeAgo = () => {
-        return time.format(parseInt(props.timestamp));
+    const renderAuthor = () => {
+      return (
+          <>
+              {showIRI ?
+                  <>
+                      {getAuthorLabel()}
+                      <LinkIcon iconClassContainer="emphasise-on-relevant-icon" url={getAuthorIRI()}/>
+                  </>
+                  : getAuthorLabel()
+              }
+          </>
+      )
+    };
+
+    const renderTimeAgo = () => {
+        const getTimeAgoFormat = () => {
+            return time.format(parseInt(props.timestamp));
+        }
+
+        const getUTCFormat = () => {
+            return new Date(parseInt(props.timestamp)).toUTCString();
+        }
+
+        return <IconOverlay id="exact-time-overlay" tooltipContent={getUTCFormat()}>
+          {getTimeAgoFormat()}
+      </IconOverlay>
+
     }
 
-    const onMouseEventHandler = () => {
+    const onMouseAuthorEventHandler = () => {
         setShowIRI(!showIRI);
     }
 
     return (
         <div className="comment-content">
             <div className="row">
-                <span className="col-auto comment-author" onMouseEnter={onMouseEventHandler} onMouseLeave={onMouseEventHandler}>
-                    {showIRI ? getAuthorIRI() : getAuthorLabel()}
+                <span className="col-auto comment-author" onMouseEnter={onMouseAuthorEventHandler} onMouseLeave={onMouseAuthorEventHandler}>
+                    {renderAuthor()}
                 </span>
-                <span className="col-auto text-muted comment-timestamp">{getTimeAgo()}</span>
+                <span className="col-auto text-muted comment-timestamp">{renderTimeAgo()}</span>
             </div>
             <div className="row">
                 <span className="col comment-value">{props.commentValue}</span>
             </div>
         </div>
     );
-}
+};
+
+CommentView.propTypes = {
+    author: PropTypes.object.isRequired,
+    timestamp: PropTypes.string.isRequired,
+    commentValue: PropTypes.string.isRequired
+};
 
 export default CommentView;
