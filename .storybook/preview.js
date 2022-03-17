0365156a8f52fb696cc222ac4a06f4f7104cb812
Max Chopart <max.chopart@gmail.com>
2022-03-17 09:16:50 +0100

    [Upd #115] Remove Globals

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,6 @@
 import 'react-datepicker/dist/react-datepicker.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/styles/s-forms.css';
-import Constants from '../src/constants/Constants';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -10,34 +9,5 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/
     }
-  }
-};
-
-export const globalTypes = {
-  iconBehavior: {
-    name: 'Icon behavior',
-    description: 'Set the behavior for the icons',
-    defaultValue: Constants.ICON_BEHAVIOR.ON_HOVER,
-    options: [Constants.ICON_BEHAVIOR.ON_HOVER, Constants.ICON_BEHAVIOR.ENABLE, Constants.ICON_BEHAVIOR.DISABLE],
-    control: { type: 'radio' }
-  },
-  locale: {
-    name: 'Locale',
-    description: 'Internationalization locale',
-    defaultValue: 'en',
-    options: ['en', 'cs'],
-    control: { type: 'radio' }
-  },
-  debugMode: {
-    name: 'Debug Mode',
-    description: 'Show irrelevant questions',
-    defaultValue: false,
-    control: { type: 'boolean' }
-  },
-  startingQuestionId: {
-    name: 'Starting question ID',
-    description: 'Set question to jump on',
-    defaultValue: 'layout-options-65',
-    control: { type: 'text' }
   }
 };
