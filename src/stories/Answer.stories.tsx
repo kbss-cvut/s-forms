6804d6e1bce077fc9880b58c3a2ea347d935d417
Max Chopart <max.chopart@gmail.com>
2022-03-17 17:06:06 +0100

    [New #115] Add stories to Answer story

--- src/stories/Answer.stories.tsx
+++ src/stories/Answer.stories.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import Answer from "../components/Answer";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import question from "./assets/questionWithMedia.json";
-import questionWithMultipleChoice from "./assets/questionWithMultipleChoice.json";
+import questionTypeHead from "./assets/questionTypeHead.json";
+import questionDate from "./assets/questionDate.json";
+import questionCheckBox from "./assets/questionCheckBox.json";
+import questionMaskedInput from "./assets/questionMaskedInput.json";
+import questionSparqlInput from "./assets/questionSparqlInput.json";
+import questionTurtleInput from "./assets/questionTurtleInput.json";
 import { ConfigurationContextProvider } from "../contexts/ConfigurationContext";
 import Constants from "../constants/Constants";
 import { FormGenContextProvider } from "../contexts/FormGenContext";
 import possibleValues from "./assets/possibleValues.json";
-import IntlContextProvider from "../contexts/IntlContextProvider";
 
 export default {
   title: "Components/Answer",
@@ -16,43 +20,9 @@ export default {
 
 const Template: ComponentStory<typeof Answer> = (args) => {
   const options = {
-    i18n: {
-      "wizard.next": "Next",
-      "wizard.previous": "Previous",
-      "section.expand": "Expand",
-      "section.collapse": "Collapse",
-    },
     intl: {
       locale: Constants.LANG.cs.locale,
     },
-    modalView: false,
-    horizontalWizardNav: false,
-    wizardStepButtons: true,
-    enableForwardSkip: true,
-    startingStep: 1,
-    debugMode: false,
-    users: [
-      { id: "http://fel.cvut.cz/people/max-chopart", label: "Max Chopart" },
-      {
-        id: "http://fel.cvut.cz/people/miroslav-blasko",
-        label: "Miroslav Blasko",
-      },
-    ],
-    currentUser: "http://fel.cvut.cz/people/max-chopart",
-    icons: [
-      {
-        id: Constants.ICONS.QUESTION_HELP,
-        behavior: Constants.ICON_BEHAVIOR.ENABLE,
-      },
-      {
-        id: Constants.ICONS.QUESTION_LINK,
-        behavior: Constants.ICON_BEHAVIOR.ENABLE,
-      },
-      {
-        id: Constants.ICONS.QUESTION_COMMENTS,
-        behavior: Constants.ICON_BEHAVIOR.ENABLE,
-      },
-    ],
   };
 
   const fetchTypeAheadValues = () => {
@@ -64,16 +34,19 @@ const Template: ComponentStory<typeof Answer> = (args) => {
   return (
     <FormGenContextProvider fetchTypeAheadValues={fetchTypeAheadValues}>
       <ConfigurationContextProvider options={options}>
-        <IntlContextProvider>
-          <Answer {...args} onChange={() => {}} onCommentChange={() => {}} />
-        </IntlContextProvider>
+        <Answer
+          {...args}
+          answer={{}}
+          onChange={() => {}}
+          onCommentChange={() => {}}
+        />
       </ConfigurationContextProvider>
     </FormGenContextProvider>
   );
 };
 
-export const Text = Template.bind({});
-Text.args = {
+export const RegularInput = Template.bind({});
+RegularInput.args = {
   answer: {
     "http://onto.fel.cvut.cz/ontologies/documentation/has_data_value": "",
   },
@@ -82,6 +55,30 @@ Text.args = {
 
 export const TypeHead = Template.bind({});
 TypeHead.args = {
-  answer: {},
-  question: questionWithMultipleChoice,
+  question: questionTypeHead,
+};
+
+export const DateTimePicker = Template.bind({});
+DateTimePicker.args = {
+  question: questionDate,
+};
+
+export const CheckBox = Template.bind({});
+CheckBox.args = {
+  question: questionCheckBox,
+};
+
+export const MaskedInput = Template.bind({});
+MaskedInput.args = {
+  question: questionMaskedInput,
+};
+
+export const SparqlInput = Template.bind({});
+SparqlInput.args = {
+  question: questionSparqlInput,
+};
+
+export const TurtleInput = Template.bind({});
+TurtleInput.args = {
+  question: questionTurtleInput,
 };
--- src/stories/PrefixIcon.stories.tsx
+++ src/stories/PrefixIcon.stories.tsx
@@ -21,12 +21,21 @@ Default.args = {
   prefixes: [
     {
       "@id": "_:b19",
+      "shacl:prefix": "form-ecc-lib",
+      "@type": "shacl:PrefixDeclaration",
+      "shacl:namespace": "http://onto.fel.cvut.cz/ontologies/aviation/eccairs-form-lib/",
     },
     {
+      "shacl:prefix": "form-ecc-cfg-0.2",
+      "shacl:namespace": "http://onto.fel.cvut.cz/ontologies/aviation/eccairs-form-config-0.2/",
       "@id": "_:b20",
+      "@type": "shacl:PrefixDeclaration"
     },
     {
+      "@type": "shacl:PrefixDeclaration",
       "@id": "_:b22",
+      "shacl:namespace": "http://www.semanticweb.org/owl/owlapi/turtle#",
+      "shacl:prefix": "turtle"
     },
   ],
   iconClass: "help-icon-checkbox",
