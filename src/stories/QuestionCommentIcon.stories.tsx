f8f8473e9c479673d7203bbc799d45ad661a99d1
Max Chopart <max.chopart@gmail.com>
2022-03-29 10:29:06 +0200

    [New #113] Add interaction test

--- src/stories/QuestionCommentIcon.stories.tsx
+++ src/stories/QuestionCommentIcon.stories.tsx
@@ -3,7 +3,8 @@ import QuestionCommentIcon from "../components/comment/QuestionCommentIcon";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import IntlContextProvider from "../contexts/IntlContextProvider";
 import { ConfigurationContextProvider } from "../contexts/ConfigurationContext";
-import { screen, userEvent, within } from "@storybook/testing-library";
+import { userEvent, within } from "@storybook/testing-library";
+import { expect } from '@storybook/jest';
 
 import question from "./assets/question/question.json";
 import questionWithComment from "./assets/question/questionWithComment.json";
@@ -44,11 +45,6 @@ Default.args = {
   question: question,
 };
 
-export const WithComment = Template.bind({});
-WithComment.args = {
-  question: questionWithComment,
-};
-
 export const TooltipOpen = Template.bind({});
 TooltipOpen.args = {
   question: question,
@@ -57,3 +53,21 @@ TooltipOpen.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
   await userEvent.click(canvas.getByTestId("comment-bubble"));
 };
+
+export const WithComment = Template.bind({});
+WithComment.args = {
+  question: questionWithComment,
+};
+
+export const TooltipOpenWithComment = Template.bind({});
+TooltipOpenWithComment.args = {
+  question: questionWithComment,
+};
+
+TooltipOpenWithComment.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await userEvent.click(canvas.getByTestId("comment-bubble"));
+  await expect(canvas.getByRole('tooltip')).toBeInTheDocument();
+  await expect(canvas.getByText("Hello, my name is Max Chopart")).toBeInTheDocument();
+};
+
