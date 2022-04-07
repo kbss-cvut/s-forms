d5e854a7599a3e867ee662d5a243985684e9c354
Max Chopart <max.chopart@gmail.com>
2022-04-07 14:25:56 +0200

    [Test #5] Test publish to npm

--- src/styles/icons/index.js
+++ src/styles/icons/index.js
@@ -3,4 +3,3 @@ export { default as CaretSquareDown } from './CaretSquareDown';
 export { default as InfoCircle } from './InfoCircle';
 export { default as QuestionCircle } from './QuestionCircle';
 export { default as Close } from './Close';
-export { default as RecycleBin } from './RecycleBin';
