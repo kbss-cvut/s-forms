56848b0ed0b3186dd56c0a8691d0ee44a434fba8
Max Chopart <max.chopart@gmail.com>
2022-08-25 16:26:30 +0200

    [New #171] Add stories for Question Component

--- src/stories/Question.stories.tsx
+++ src/stories/Question.stories.tsx
@@ -1,76 +1,75 @@
-import React, { useContext } from "react";
+import React from "react";
 import Question from "../components/Question";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ConfigurationContext } from "../contexts/ConfigurationContext";
+import DefaultInput from "../components/DefaultInput";
 
-const questionExpanded = {
-  "@id":
-    "http://onto.fel.cvut.cz/ontologies/eccairs/model/instance#instance-1223764187-q",
-  "@type": "doc:question",
-  "http://onto.fel.cvut.cz/ontologies/documentation/has_answer": {
-    "@id":
-      "http://onto.fel.cvut.cz/ontologies/eccairs/model/instance#instance-636079211-a",
-    "http://onto.fel.cvut.cz/ontologies/documentation/has_data_value": "test",
-  },
-  "http://onto.fel.cvut.cz/ontologies/form/has-origin-type":
-    "http://onto.fel.cvut.cz/ontologies/eccairs/aviation-3.4.0.2/a-453",
-  "http://onto.fel.cvut.cz/ontologies/form/has-template-origin":
-    "http://onto.fel.cvut.cz/ontologies/eccairs/model/instance#instance-1223764187",
-  "http://onto.fel.cvut.cz/ontologies/form/has-template":
-    "http://onto.fel.cvut.cz/ontologies/eccairs/aviation-3.4.0.2/a-453-qt",
-  "http://www.w3.org/2000/01/rdf-schema#comment": {
-    "@language": "en",
-    "@value":
-      "The identification of the entity or organisation that is responsible for the report.",
-  },
-  "http://www.w3.org/2000/01/rdf-schema#label": {
-    "@language": "en",
-    "@value": "453 - Responsible entity",
-  },
-  "http://onto.fel.cvut.cz/ontologies/form-layout/has-layout-class": "section",
-};
-const questionCollapsed = {
-  "@id":
-    "http://onto.fel.cvut.cz/ontologies/eccairs/model/instance#instance-1223764187-q",
-  "@type": "doc:question",
-  "http://onto.fel.cvut.cz/ontologies/documentation/has_answer": {
-    "@id":
-      "http://onto.fel.cvut.cz/ontologies/eccairs/model/instance#instance-636079211-a",
-    "http://onto.fel.cvut.cz/ontologies/documentation/has_data_value": "test",
-  },
-  "http://onto.fel.cvut.cz/ontologies/form/has-origin-type":
-    "http://onto.fel.cvut.cz/ontologies/eccairs/aviation-3.4.0.2/a-453",
-  "http://onto.fel.cvut.cz/ontologies/form/has-template-origin":
-    "http://onto.fel.cvut.cz/ontologies/eccairs/model/instance#instance-1223764187",
-  "http://onto.fel.cvut.cz/ontologies/form/has-template":
-    "http://onto.fel.cvut.cz/ontologies/eccairs/aviation-3.4.0.2/a-453-qt",
-  "http://www.w3.org/2000/01/rdf-schema#comment": {
-    "@language": "en",
-    "@value":
-      "The identification of the entity or organisation that is responsible for the report.",
-  },
-  "http://www.w3.org/2000/01/rdf-schema#label": {
-    "@language": "en",
-    "@value": "453 - Responsible entity",
-  },
-  "http://onto.fel.cvut.cz/ontologies/form-layout/has-layout-class": [
-    "section",
-    "collapsed",
-  ],
-};
+import questionExpanded from "./assets/question/questionExpanded.json";
+import questionCollapsed from "./assets/question/questionCollapsed.json";
+import showHiddenQuestion from "./assets/question/showHiddenQuestion.json";
+import hiddenQuestion from "./assets/question/hiddenQuestion.json";
 
 export default {
   title: "Components/Question",
   component: Question,
 } as ComponentMeta<typeof Question>;
 
-const Template: ComponentStory<typeof Question> = (args) => {
-  return <Question {...args} onChange={{}} />;
+const Template: ComponentStory<typeof Question> = (
+  args,
+  { globals: { debugMode } }
+) => {
+  const inputComponent = DefaultInput;
+  const componentsOptions = {
+    readOnly: false,
+    dateTimeAnswer: {
+      dateFormat: "yyyy-MM-dd",
+      timeFormat: "HH:mm:ss",
+      dateTimeFormat: "yyyy-MM-dd HH:mm:ss",
+    },
+  };
+  const options = {
+    intl: {
+      locale: "en",
+    },
+    i18n: {},
+    debugMode: debugMode,
+  };
+  const _getComponentMappingFunction = () => {
+    return (question: any, defaultComponent: any) => {
+      return defaultComponent;
+    };
+  };
+  const mapComponent = _getComponentMappingFunction();
+
+  return (
+    <ConfigurationContext.Provider
+      value={{
+        componentsOptions,
+        inputComponent,
+        options,
+        mapComponent,
+      }}
+    >
+      <Question {...args} />
+    </ConfigurationContext.Provider>
+  );
+};
+
+export const HiddenQuestion = Template.bind({});
+HiddenQuestion.args = {
+  question: hiddenQuestion,
+};
+
+export const ShowHiddenQuestion = Template.bind({});
+ShowHiddenQuestion.args = {
+  question: showHiddenQuestion,
 };
 
 export const CollapsedQuestion = Template.bind({});
 CollapsedQuestion.args = {
   question: questionCollapsed,
 };
+
 export const ExpandedQuestion = Template.bind({});
 ExpandedQuestion.args = {
   question: questionExpanded,
