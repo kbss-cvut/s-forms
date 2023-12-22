bbdf088a7f046491e29d6669068b7784d3edb713
Max Chopart <max.chopart@gmail.com>
2023-12-22 17:23:50 +0100

    [Fix #247] When navigating across wizard, value of tree is now kept

--- src/components/answer/TypeaheadAnswer.jsx
+++ src/components/answer/TypeaheadAnswer.jsx
@@ -129,7 +129,7 @@ const TypeaheadAnswer = (props) => {
       <IntelligentTreeSelect
         valueKey={valueKey}
         labelKey={labelKey}
-        valueIsControlled={false}
+        valueIsControlled={true}
         value={optionsList.filter((option) => option.id === props.value)}
         multi={false}
         options={optionsList}
