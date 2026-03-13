4ea1770b47a34d0e717465ed94c2b177534ff2c8
Yevgeniy <ulchenkov2001@gmail.com>
2026-03-13 11:45:01 +0100

    [upd] code refactored for typehead and select. Added controller.

--- src/components/answer/typehead/TypeaheadAnswer.jsx
+++ src/components/answer/typehead/TypeaheadAnswer.jsx
@@ -1,79 +1,70 @@
 import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 
-import Constants from "../../../constants/Constants.js";
-import FormUtils from "../../../util/FormUtils.js";
-import Utils from "../../../util/Utils.js";
-import Logger from "../../../util/Logger.js";
+import Constants from "../../../constants/Constants";
+import FormUtils from "../../../util/FormUtils";
+import Utils from "../../../util/Utils";
+import Logger from "../../../util/Logger";
 
-import { FormGroup, Form } from "react-bootstrap";
-import { FormGenContext } from "../../../contexts/FormGenContext.js";
-import { ConfigurationContext } from "../../../contexts/ConfigurationContext.js";
-import { IntelligentTreeSelect } from "intelligent-tree-select/lib/components/IntelligentTreeSelect.js";
+import { FormGenContext } from "../../../contexts/FormGenContext";
+import { ConfigurationContext } from "../../../contexts/ConfigurationContext";
+
 import { useIntl } from "react-intl";
 
-import "intelligent-tree-select/lib/styles.css";
+import { buildOptionsTree } from "./typeahead.utils";
 
-import { buildOptionsTree } from "./typeahead.utils.js";
-import { evaluateAnswer } from "./typeahead.feedback.js";
-import { buildControlStyle } from "./typeahead.styles.js";
-import {
-  createOptionRenderer,
-  valueRendererWithFeedback,
-} from "./typeahead.renderers.jsx";
+import TypeaheadDefault from "./TypeheadDefault";
+import TypeaheadWithFeedback from "./TypeaheadWithFeedback";
+import "intelligent-tree-select/lib/styles.css";
 
 const TypeaheadAnswer = (props) => {
   const intl = useIntl();
+
   const formGenContext = useContext(FormGenContext);
   const configurationContext = useContext(ConfigurationContext);
-  const feedbackEnabled = Utils.isOptionQuestionFeedbackEnabled(
-    configurationContext?.options
-  );
 
   const queryHash = Utils.getStringHash(
     FormUtils.getPossibleValuesQuery(props.question)
   );
 
-  const [feedback, setFeedback] = useState(null);
-  const [selectedId, setSelectedId] = useState(null);
-  const [previousSelectedId, setPreviousSelectedId] = useState(null);
-  const [optionsList, setOptionsList] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [optionsList, setOptionsList] = useState([]);
 
-  const valueKey = Utils.findKeyInObjects(optionsList, ["name", "value"]);
-  const labelKey = Utils.findKeyInObjects(optionsList, ["name", "label"]);
+  const learningEnabled = Utils.isOptionQuestionFeedbackEnabled(
+    configurationContext?.options
+  );
 
   useEffect(() => {
     let isCancelled = false;
-    const question = props.question;
 
-    const applyOptions = (rawOptions) => {
-      if (isCancelled) return;
-      setOptionsList(buildOptionsTree(rawOptions, intl));
-      setLoading(false);
-    };
-
-    const loadFormOptions = async () => {
+    async function loadOptions() {
       try {
         const options = await formGenContext.loadFormOptions(
           queryHash,
-          FormUtils.getPossibleValuesQuery(question)
+          FormUtils.getPossibleValuesQuery(props.question)
         );
-        applyOptions(options);
+
+        if (!isCancelled) {
+          setOptionsList(buildOptionsTree(options, intl));
+          setLoading(false);
+        }
       } catch (error) {
         Logger.error(
-          `TypeaheadAnswer: loadFormOptions failed for query hash: ${queryHash}`
+          `Typeahead: options loading failed for query hash ${queryHash}`
         );
       }
-    };
+    }
 
     if (
-      !question[Constants.HAS_OPTION] &&
-      FormUtils.getPossibleValuesQuery(question)
+      !props.question[Constants.HAS_OPTION] &&
+      FormUtils.getPossibleValuesQuery(props.question)
     ) {
-      loadFormOptions();
+      loadOptions();
     } else {
-      applyOptions(question[Constants.HAS_OPTION]);
+      setOptionsList(
+        buildOptionsTree(props.question[Constants.HAS_OPTION], intl)
+      );
+      setLoading(false);
     }
 
     return () => {
@@ -81,103 +72,29 @@ const TypeaheadAnswer = (props) => {
     };
   }, [queryHash, formGenContext, props.question, intl]);
 
-  useEffect(() => {
-    if (!props.hintRevealed || !optionsList.length) return;
+  const valueKey = Utils.findKeyInObjects(optionsList, ["name", "value"]);
+  const labelKey = Utils.findKeyInObjects(optionsList, ["name", "label"]);
 
-    const correctValue = FormUtils.getCorrectAnswerValue(props.question);
-    const correctOption = optionsList.find(
-      (o) => o.label.trim() === correctValue?.trim()
-    );
-
-    if (!correctOption) {
-      Logger.warn(
-        `TypeaheadAnswer: correct option not found for value "${correctValue}"`
-      );
-      return;
-    }
-
-    setSelectedId(correctOption.id);
-    setFeedback("correct");
-    props.onChange(correctOption.id);
-  }, [props.hintRevealed, optionsList]);
-
-  const handleChange = (option) => {
-    if (props.hintRevealed) return;
-
-    const id = option?.id ?? null;
-
-    setSelectedId(id);
-    setPreviousSelectedId(id);
-    props.onChange(id);
-
-    if (feedbackEnabled) {
-      setFeedback(evaluateAnswer(option?.label, props.question));
-    }
+  const sharedProps = {
+    ...props,
+    optionsList,
+    valueKey,
+    labelKey,
+    isLoading,
+    configurationContext,
   };
 
-  const selectedOption =
-    optionsList.find((o) => o.id === (selectedId ?? props.value)) || null;
+  if (learningEnabled) {
+    return <TypeaheadWithFeedback {...sharedProps} />;
+  }
 
-  useEffect(() => {
-    if (!optionsList.length) return;
-    if (!feedbackEnabled) return;
-    if (!FormUtils.hasAnswer(props.question)) return;
-
-    const existingValue = props.value;
-    if (!existingValue) return;
-
-    const matchedOption = optionsList.find((o) => o.id === existingValue);
-    if (!matchedOption) return;
-
-    setSelectedId(matchedOption.id);
-    setPreviousSelectedId(matchedOption.id);
-    setFeedback(evaluateAnswer(matchedOption.label, props.question));
-  }, [optionsList]);
-
-  return (
-    <FormGroup size="small" className="mb-3">
-      <Form.Label>{props.label}</Form.Label>
-      <IntelligentTreeSelect
-        key={selectedId ?? props.value ?? "empty"}
-        valueIsControlled
-        optionRenderer={createOptionRenderer({
-          feedback,
-          previousSelectedId,
-          question: props.question,
-        })}
-        styles={buildControlStyle(props.validation, feedback)}
-        valueKey={valueKey}
-        labelKey={labelKey}
-        valueRenderer={(children) =>
-          valueRendererWithFeedback(feedback, children)
-        }
-        value={selectedOption}
-        multi={false}
-        options={optionsList}
-        isSearchable
-        isLoading={isLoading}
-        isClearable
-        renderAsTree
-        optionLeftOffset={5}
-        isDisabled={
-          isLoading ||
-          configurationContext.componentsOptions.readOnly ||
-          FormUtils.isDisabled(props.question)
-        }
-        onChange={handleChange}
-        titleKey="description"
-      />
-
-      {props.validation?.message}
-    </FormGroup>
-  );
+  return <TypeaheadDefault {...sharedProps} />;
 };
 
 TypeaheadAnswer.propTypes = {
   question: PropTypes.object.isRequired,
   answer: PropTypes.object.isRequired,
   label: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
-  title: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func.isRequired,
   validation: PropTypes.object,
--- src/components/answer/typehead/typeahead.renderers.jsx
+++ src/components/answer/typehead/typeahead.renderers.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Option from "intelligent-tree-select/lib/components/Option";
 import FormUtils from "../../../util/FormUtils.js";
 
 const FEEDBACK_COLORS = {
@@ -6,54 +7,75 @@ const FEEDBACK_COLORS = {
   incorrect: "#f8d7da",
 };
 
+const VALIDATION_STYLES = {
+  "is-warning": {
+    borderColor: "#dc9135",
+    boxShadow: "0 0 0 0.2rem rgba(203,127,16,.25)",
+  },
+  "is-invalid": {
+    borderColor: "#dc3545",
+    boxShadow: "0 0 0 0.2rem rgba(220,53,69,.25)",
+  },
+};
+
+const FEEDBACK_STYLES = {
+  correct: {
+    borderColor: "#28a745",
+    boxShadow: "0 0 0 0.2rem rgba(40,167,69,.25)",
+  },
+  incorrect: {
+    borderColor: "#dc3545",
+    boxShadow: "0 0 0 0.2rem rgba(220,53,69,.25)",
+  },
+};
+
+const buildControlOverride = ({ borderColor, boxShadow } = {}) => ({
+  control: (provided) => ({
+    ...provided,
+    border: borderColor ? `1px solid ${borderColor}` : provided.border,
+    boxShadow: boxShadow ?? provided.boxShadow,
+    "&:hover": {
+      border: borderColor ? `1px solid ${borderColor}` : provided.border,
+      boxShadow: boxShadow ?? provided.boxShadow,
+    },
+  }),
+});
+
+export const buildControlStyle = (validation, feedback) => {
+  const styles =
+    VALIDATION_STYLES[validation?.classname] ?? FEEDBACK_STYLES[feedback] ?? {};
+  return buildControlOverride(styles);
+};
+
 export const createOptionRenderer =
-  ({ feedback, previousSelectedId, question }) =>
+  ({ hintRevealed, correctValue, currentValue }) =>
   (optionProps) => {
-    const correctValue = FormUtils.getCorrectAnswerValue(question);
+    const isCorrect = optionProps.label?.trim() === correctValue?.trim();
+    const isCurrent = optionProps.label?.trim() === currentValue?.label?.trim();
 
     let background = "transparent";
-
-    const isCorrectOption = optionProps.label === correctValue;
-    const wasSelectedWrong =
-      optionProps.data.id === previousSelectedId && !isCorrectOption;
-    const isSelectedOption = optionProps.data.id === previousSelectedId;
-
-    if (feedback === "correct" && isCorrectOption) {
-      background = "#d4edda";
-    }
-
-    if (feedback === "correct" && wasSelectedWrong) {
-      background = "#f8d7da";
-    }
-
-    if (feedback === "incorrect" && isSelectedOption) {
-      background = "#f8d7da";
-    }
+    if (hintRevealed && isCorrect) background = FEEDBACK_COLORS.correct;
+    if (isCurrent)
+      background = isCorrect
+        ? FEEDBACK_COLORS.correct
+        : FEEDBACK_COLORS.incorrect;
 
     return (
-      <div
-        style={{
-          backgroundColor: background,
-          cursor: "pointer",
-        }}
-        onClick={() => optionProps.selectOption(optionProps.data)}
-      >
-        {optionProps.label}
+      <div style={{ backgroundColor: background }}>
+        <Option {...optionProps} />
       </div>
     );
   };
 
-export const valueRendererWithFeedback = (feedback, children) => {
-  return (
-    <div
-      style={{
-        backgroundColor: FEEDBACK_COLORS[feedback] ?? "transparent",
-        padding: "2px 6px",
-        borderRadius: "4px",
-        display: "inline-block",
-      }}
-    >
-      {children}
-    </div>
-  );
-};
+export const valueRendererWithFeedback = (feedback, children) => (
+  <div
+    style={{
+      backgroundColor: FEEDBACK_COLORS[feedback] ?? "transparent",
+      padding: "2px 6px",
+      borderRadius: "4px",
+      display: "inline-block",
+    }}
+  >
+    {children}
+  </div>
+);
